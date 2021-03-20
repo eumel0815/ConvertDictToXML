e41067d0bc1200e7895223cf0152f34f4132a270
eumel0815 <danystraub@outlook.de>
2021-03-20 22:42:17 +0100

    Erstellung

--- setup.py
+++ setup.py
@@ -13,12 +13,9 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/user/reponame',
     # I explain this later on
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    download_url='https://github.com/eumel0815/ConvertDictToXML/archive/refs/tags/0.1.tar.gz',
     keywords=['Dictionary', 'XML'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
-        'validators',
-        'beautifulsoup4',
-    ],
+    install_requires=[],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
